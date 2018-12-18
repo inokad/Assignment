ad350920ba49bc14512267209b49ec2bbdaf1ada
Inoka Dissanayaka <inokadissanayaka@inokas-mbp.lan>
2018-12-18 22:07:12 +0800

    Initial Commit

---
+++ SPHAssignmentTests/SPHAssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  SPHAssignmentTests.swift
+//  SPHAssignmentTests
+//
+//  Created by Inoka Dissanayaka on 12/18/18.
+//  Copyright © 2018 Inoka Dissanayaka. All rights reserved.
+//
+
+import XCTest
+@testable import SPHAssignment
+
+class SPHAssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
